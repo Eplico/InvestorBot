534c83d53b11cfc38eb8ccb6bdf5bb45d7dcd0ce
Eplico <33384110+Eplico@users.noreply.github.com>
2019-05-02 15:30:38 -0600

    Add files via upload

--- NotifyServerV1.0.py
+++ NotifyServerV1.0.py
@@ -3,13 +3,13 @@ import praw
 import time
 import requests
 import os
-heroku buildpacks:set heroku/python
 
 authors = [
-    'organic_crystal_meth', 'SlothySurprise', 'bleach_tastes_bad', 'lukenamop', 'blkmmb', 'iscatmypants'
+    'organic_crystal_meth', 'SlothySurprise', 'bleach_tastes_bad', 'lukenamop', 'blkmmb', 'iscatmypants', 't3karnOnYoAzz'
 ]
-MGDWU = 'https://discordapp.com/api/webhooks/562009771587272724/S_lNTtWogUbnyhKH8HIPQTPI2OxdXSD2_hypCVTxi2SKBAbPPX9SC4Tg7aK_ZHX0YtGW'
-#TSDWU = 'https://discordapp.com/api/webhooks/565854496157335573/grLVILfHxOjP-aBB0PHvInH7GGU709MDURJYGz8Q70Bj0mV0KZFDk5bagCft1oTma3PS'
+# discord webhook url as variable for ease
+DWU = 'https://discordapp.com/api/webhooks/562009771587272724/S_lNTtWogUbnyhKH8HIPQTPI2OxdXSD2_hypCVTxi2SKBAbPPX9SC4Tg7aK_ZHX0YtGW'
+
 message = " Has Posted!! Go Invest Now, Before Its Too Late!! "
 
 
@@ -21,34 +21,43 @@ def authenticate():
 
 
 def run_bot(reddit, PostsNotified):
-    print('Looking for posts by {}...'.format(authors))
+    print('Looking for posts')
     subreddit = reddit.subreddit('MemeEconomy')
     for submission in subreddit.new(limit=5):
-
+        # Check the subreddit for new posts by specified authors and that you haven't scanned that post yet
         if submission.author in authors and submission.id not in PostsNotified:
             print('Found Post By {}...'.format(submission.author))
+            # build url to post
             suburl = 'https://www.reddit.com/r/{}/comments/{}'.format(submission.subreddit, submission.id)
+            # build message to be sent with gathered info
             data = {"content": "{} {} {}".format(submission.author, message, suburl)}
-            requests.post(MGDWU, data=data)
+            # send message
+            requests.post(DWU, data=data)
+            
             print('Notifying Discord Users...')
+            # put post id in memory so you don't catch it again
             PostsNotified.append(submission.id)
 
             with open("PostsNotified.txt", "a") as f:
                 f.write(submission.id + "\n")
 
-    print('Sleeping For 50 Seconds....')
-    time.sleep(50)
+    print('Sleeping For 1 Minute....')
+    # wait 1 minute to scan for new posts
+    time.sleep(60)
 
 
 def CheckNotified():
+    # if the file isn't there, make it
     if not os.path.isfile('PostsNotified.txt'):
         PostsNotified = []
     else:
+        # if it is, read it
         with open('PostsNotified.txt', 'r') as f:
             PostsNotified = f.read()
             PostsNotified = PostsNotified.split("\n")
             PostsNotified = list(filter(None, PostsNotified))
     return PostsNotified
+# run the script
 
 
 reddit = authenticate()
